6c219341d0493a0a7f52db9f5d3a84b24ca3674f
Srijan Samridh <srijan.samridh@coindcx.com>
2024-07-24 00:15:22 +0530

    commented unwanted wallets

--- packages/okto-cosmos-app/components/wallet.provider.js
+++ packages/okto-cosmos-app/components/wallet.provider.js
@@ -2,13 +2,13 @@
 
 import { ChainProvider } from '@cosmos-kit/react';
 import { chains, assets } from 'chain-registry';
-import { wallets as keplrWallets } from '@cosmos-kit/keplr';
-import { wallets as exodusWallets } from '@cosmos-kit/exodus';
-import { wallets as coin98Wallets } from '@cosmos-kit/coin98-extension';
+// import { wallets as keplrWallets } from '@cosmos-kit/keplr';
+// import { wallets as exodusWallets } from '@cosmos-kit/exodus';
+// import { wallets as coin98Wallets } from '@cosmos-kit/coin98-extension';
 import { wallets as oktoWallets } from '../../../wallets/okto-extension';
 import { wallets as leapWallets } from "../../../wallets/leap-extension";
-import { wallets as cosmostationWallets } from "@cosmos-kit/cosmostation";
-import { wallets as finWallets } from "@cosmos-kit/fin";
+// import { wallets as cosmostationWallets } from "@cosmos-kit/cosmostation";
+// import { wallets as finWallets } from "@cosmos-kit/fin";
 
 import "@interchain-ui/react/styles";
 
